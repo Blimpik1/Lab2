23eac5511de8b6fc43c7d04c4971380dab903226
admin <admin@DESKTOP-PI451DB>
2024-10-11 13:01:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+      
+        int[] array = { 9, 3, 6, 1, 7, 5, 8, 2, 10, 4 };
+
+        Console.WriteLine("Масив:");
+        foreach (int num in array)
+        {
+            Console.Write(num);
+        }
+
+        Array.Sort(array);
+
+        Console.WriteLine("\nМасив після сортування:");
+        foreach (int num in array)
+        {
+            Console.Write(num);
+        }
+    }
+}
+
