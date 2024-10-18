9163c73961ea29e1b8206566f4c3f3ee6c9546d7
Blimpik1 <151070934+Blimpik1@users.noreply.github.com>
2024-10-18 11:23:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
-﻿using System;
+using System;
 
 class Program
 {
-
     static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-      
-        int[] array = { 9, 3, 6, 1, 7, 5, 8, 2, 10, 4 };
 
+        int[] array = { 9, 3, 6, 1, 7, 5, 8, 2, 10, 4 };
         Console.WriteLine("Масив:");
         foreach (int num in array)
         {
             Console.Write(num);
         }
 
-        Array.Sort(array);
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = 0; j < array.Length - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
 
         Console.WriteLine("\nМасив після сортування:");
         foreach (int num in array)
@@ -24,4 +33,3 @@ class Program
         }
     }
 }
-
